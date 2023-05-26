3f4784b06788574fe5b46728e86d172135e06818
marinh0w <gabrielgambardella14@gmail.com>
2023-05-26 18:25:16 -0300

    finalizado estrutura da home, não funciona

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,30 @@
+import Image from "next/image";
+import { HomeContainer, Product } from "@/styles/pages/home";
+
+import imagem1 from "../assets/1.png";
+import imagem2 from "../assets/2.png";
+import imagem3 from "../assets/3.png";
+
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <HomeContainer>
+      <Product>
+        <Image src={imagem1} width={520} height={480} alt={""} />
+
+        <footer>
+          <strong>Imagem X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={imagem1} width={520} height={480} alt={""} />
+
+        <footer>
+          <strong>Imagem X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  );
 }
