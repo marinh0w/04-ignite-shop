250ee51d652829d07bbad1575f851cb158503b81
marinh0w <gabrielgambardella14@gmail.com>
2023-06-06 14:50:19 -0300

    fim do projeto não funciona

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { GetServerSideProps, GetStaticProps } from "next";
+import { GetStaticProps } from "next";
+import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -33,22 +34,37 @@ export default function Home({ products }) {
   });
 
   return (
-    <HomeContainer ref={sliderRef} className="keen-slider">
-      {products.map((product) => {
-        return (
-          <Link href={"/product/${product.id}"} key={product.id} prefetch={false}>
-            <Product className="keen-slider__slide">
-              <Image src={product.imageUrl} width={520} height={480} alt={""} />
+    <>
+      <Head>
+        <title>Home | Ignite Shop</title>
+      </Head>
 
-              <footer>
-                <strong>{product.name}</strong>
-                <span>{product.price}</span>
-              </footer>
-            </Product>
-          </Link>
-        );
-      })}
-    </HomeContainer>
+      <HomeContainer ref={sliderRef} className="keen-slider">
+        {products.map(product => {
+          return (
+            <Link
+              href={"/product/${product.id}"}
+              key={product.id}
+              prefetch={false}
+            >
+              <Product className="keen-slider__slide">
+                <Image
+                  src={product.imageUrl}
+                  width={520}
+                  height={480}
+                  alt={""}
+                />
+
+                <footer>
+                  <strong>{product.name}</strong>
+                  <span>{product.price}</span>
+                </footer>
+              </Product>
+            </Link>
+          );
+        })}
+      </HomeContainer>
+    </>
   );
 }
 
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -45,6 +45,11 @@ export default function Product({ product }: ProductProps) {
   }
 
   return (
+    <>
+     <Head>
+        <title>{product.name} | Ignite Shop</title>
+      </Head>
+
     <ProductContainer>
       <ImageContainer>
         <Image src={product.imageUrl} width={520} height={480} alt="" />
@@ -61,7 +66,8 @@ export default function Product({ product }: ProductProps) {
         <button disabled={isCreatingCheckoutSession} onClick={handleBuyProduct}>Comprar agora</button>
       </ProductDetails>
     </ProductContainer>
-  );
+    </>
+      );  
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
